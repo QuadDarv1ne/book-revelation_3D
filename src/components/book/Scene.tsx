c30b0dcc27cc42e67cf79cf3c05150a83a7fa8af
QuadDarv1ne <maksimqwe42@mail.ru>
2026-03-05 18:58:16 +0300

    optimize: удаление неиспользуемого кода и зависимостей

--- src/components/book/Scene.tsx
+++ src/components/book/Scene.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useMemo } from "react";
 import { Sparkles, ContactShadows } from "@react-three/drei";
 import { Book } from "./Book";
 import { Podium } from "./Podium";
@@ -11,14 +12,32 @@ interface SceneProps {
 }
 
 export function Scene({ isRotating }: SceneProps) {
+  const sparklesProps = useMemo(() => ({
+    count: 20,
+    scale: 4,
+    size: 1.5,
+    speed: 0.1,
+    color: "#d4af37",
+    opacity: 0.25,
+  }), []);
+
+  const shadowProps = useMemo(() => ({
+    position: [0, -0.78, 0] as [number, number, number],
+    opacity: 0.4,
+    scale: 5,
+    blur: 2.5,
+    far: 3,
+    color: "#000",
+  }), []);
+
   return (
     <>
       <Lighting />
       <Book isRotating={isRotating} />
       <Podium />
       <ParticleRing isRotating={isRotating} />
-      <Sparkles count={25} scale={5} size={2} speed={0.12} color="#d4af37" opacity={0.3} />
-      <ContactShadows position={[0, -0.78, 0]} opacity={0.5} scale={6} blur={3} far={4} color="#000" />
+      <Sparkles {...sparklesProps} />
+      <ContactShadows {...shadowProps} />
     </>
   );
 }
